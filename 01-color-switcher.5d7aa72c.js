a0b9e85eb2d3b056127475d62bf87828cfda9941
MonikaGellert <113904942+MonikaGellert@users.noreply.github.com>
2023-07-16 21:00:16 +0000

    Deploying to gh-pages from @ MonikaGellert/goit-js-hw-09@fbf5db5490824adaff2ea89432a5bc902dca440b 🚀

---
+++ 01-color-switcher.5d7aa72c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=null;e.addEventListener("click",(function(e){a=setInterval((function(){t.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.target.disabled=!0})),n.addEventListener("click",(function(t){clearInterval(a),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5d7aa72c.js.map
