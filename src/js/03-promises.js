a90424be8e842b77eb613f0926b0376a360db4c5
MonikaGellert <monika.gellert@wp.pl>
2023-07-16 22:42:16 +0200

    test

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const formElement = document.querySelector('.form');
+
+formElement.addEventListener('submit', submitBtn);
+
+function submitBtn(event) {
+  event.preventDefault();
+  const el = event.target.elements;
+
+  const amount = el.amount.valueAsNumber;
+  const delay = el.delay.valueAsNumber;
+  const step = el.step.valueAsNumber;
+
+  if (amount < 0 || delay < 0 || step < 0) {
+    return Notiflix.Notify.warning('Cogito ergo sum');
+  }
+
+  let setTime = delay;
+  for (let i = 1; i <= amount; i += 1) {
+    if (i > 1) {
+      setTime += step;
+    }
+    createPromise(i, setTime)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
